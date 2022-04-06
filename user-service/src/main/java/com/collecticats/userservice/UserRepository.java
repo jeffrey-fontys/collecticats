e7704e5edb6e4b8e2c6e785a55c0b05d82e8107c
Jeffrey Derksen <jeffrey.derksen@student.fontys.nl>
2022-04-06 15:36:46 +0000

    User repo returns User instead of List<User>

--- user-service/src/main/java/com/collecticats/userservice/UserRepository.java
+++ user-service/src/main/java/com/collecticats/userservice/UserRepository.java
@@ -1,12 +1,10 @@
 package com.collecticats.userservice;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends MongoRepository<User, String> {
 
-  List<User> findByAuthId(@Param("authId") String id);
+  User findByAuthId(@Param("authId") String id);
 
 }
