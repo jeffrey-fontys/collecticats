5790b0c1c07e2f0a3b8b8f0a594e807dc9a4545a
Jeffrey Derksen <jeffrey.derksen@student.fontys.nl>
2022-06-13 13:37:23 +0000

    Fix for NullPointerException

--- api-gateway/src/main/java/com/collecticats/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/collecticats/apigateway/ApiGatewayApplication.java
@@ -25,6 +25,7 @@ public class ApiGatewayApplication {
 	public List<GroupedOpenApi> apis() {
 		List<GroupedOpenApi> groups = new ArrayList<>();
 		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+		assert definitions != null;
 		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
 			String name = routeDefinition.getId().replaceAll("-service", "");
 			GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
